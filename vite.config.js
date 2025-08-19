eee01cf0452829dd69e216d9eede0cb11e6c2f5a
Mohamed Abdelaal <moabdelaal442004@gmail.com>
2025-08-19 06:09:54 +0300

    Deploy: fix gh-pages cache issue

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/my-portfolio/', // ⬅ replace with your repo name exactly
+});
